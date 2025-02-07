0ff1a35da40e23acc458b1ee6085cc1cf6a8ada9
Sitaratoons <mrajayh393798@gmail.com>
2025-02-07 11:43:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,9 +3,9 @@ from os import environ
 
 API_ID       = int(environ.get("API_ID", "28918271"))
 API_HASH     = environ.get("API_HASH", "29bf447b916a795191046a91317869fb")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "7707522221:AAFEc5JiXCPydeA3bTN77a8L1M0IwtMgd-M")
 SESSION      = environ.get("SESSION" , "BQFqAQUAILF71ce55v3V4Q_RX28TKyzEz2D-B5HD0T6Q4mAuiNS1GKshZJUSfzKmGw00CqAuPQUxbOn_gxDC0qjyHTcuo30gVOuTbWL3AWnd6Z5Sef4lP_AhSxMhUuLjDNaGUHpZ0s3PBh6Z9cMXBmLMHbOj9jRHd9Tu8PfMHmTSJQ-t_uLJQSlNMmOORHD13tvMFWhBsO5NJqTvB00DclFyYRxZt5Y8L9oFajnDUBpgEAe2Q5JmUJtdUcNPsMfXa8FQ6fKlYSwXpHsCEUXoyDLiZTjcZb0DOl3Pa1uFuTTa1_R0bNSSQFB-hEcmoeuazY7YPXMelePqOulfAgGra_T3V1n-yQAAAAF24wVEAA")
 DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://sitaratoons:sitaratoons@cluster0.98nq3.mongodb.net/?retryWrites=true&w=majority")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002425566477"))
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002067741707"))
 ADMIN        = int(environ.get("ADMIN", "6692613520"))
 CHANNEL      = environ.get("CHANNEL", "ST_Rename_Update")
